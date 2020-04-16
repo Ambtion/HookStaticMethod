50e9b541b1800c80398aca1fb57e0918b1fda2ab
LinJunHou <kequ1988@gmail.com>
2020-04-16 13:06:28 +0800

    Initial Commit

---
+++ HookStaticMethod/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HookStaticMethod
+//
+//  Created by Qu,Ke on 2020/4/16.
+//  Copyright © 2020 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HookStaticMethod/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HookStaticMethod
+//
+//  Created by Qu,Ke on 2020/4/16.
+//  Copyright © 2020 baidu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
